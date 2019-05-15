bc9e29965208509be9d1a7cf976029ee20666822
Hjörtur Pálmi Pálsson <hjorturpp@ntv.is>
2019-05-15 23:48:06 +0000

    first commit

---
+++ WPF1_2/SecondWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF1_2
+{
+    /// <summary>
+    /// Interaction logic for SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Properties["newtitle"] = txtTitle.Text;
+            this.Close();
+        }
+    }
+}
